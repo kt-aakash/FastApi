18d5d959bd1c8ade60e034ac2ff571f1fb1e203b
ktaakash <ktaakash@gmail.com>
2026-04-03 20:30:03 +0530

    Products without pydantic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -7,6 +8,12 @@ app = FastAPI()
 def greet():
     return("Welcome to the track..!")
 
+products = [
+    Product(1, "phone", "budget phone", 99, 10),
+    Product(1, "laptop", "nvidia laptop", 199, 1)
+]
 
 
-
+@app.get("/products")
+def get_all_products():
+    return(products)
