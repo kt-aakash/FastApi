d53e9b41503d5e4eb05d0f133ba4cea691422451
ktaakash <ktaakash@gmail.com>
2026-04-03 21:15:52 +0530

    Produts fetch with Pydantic

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def greet():
 
 
 products = [
-    Product(1, "mobile", "budget mobile", 23.5, 3),
-    Product(2, "laptop", "nvidia laptop", 40, 5),    
+    Product(id=1, name="mobile", description="budget mobile", price=23.5, quantity=3),
+    Product(id=2, name="laptop", description="nvidia laptop", price=40, quantity=5),
+    Product(id=3, name="pen", description="pen", price=2, quantity=10),
+    Product(id=4, name="pencil", description="pencil", price=4, quantity=15)    
 ]
 
 @app.get("/products")
 def get_all_products():
-    return 
+    return products
+
+@app.get("/products/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
