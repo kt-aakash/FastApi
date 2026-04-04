7ef314ac47063df46cb341a5029e773606baa9e3
ktaakash <ktaakash@gmail.com>
2026-04-04 18:36:16 +0530

    Starting postgres part

--- main.py
+++ main.py
@@ -16,12 +16,38 @@ products = [
     Product(id=4, name="pencil", description="pencil", price=4, quantity=15)    
 ]
 
-@app.get("/products")
+@app.get("/product")
 def get_all_products():
     return products
 
-@app.get("/products/{id}")
+@app.get("/product/{id}")
 def get_product_by_id(id:int):
     for product in products:
         if product.id == id:
             return product
+        
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+
+@app.put("/product")
+def update_product(id:int, product:Product):
+    for i in range(0, len(products)-1):
+        if products[i].id == id:
+            products[i] = product
+            return "Product added successfully..!"
+    return "Product not found..!"
+
+
+
+@app.delete("/product")
+def delete_product(id:int):
+    for product in products:
+        if product.id == id:
+            products.remove(product)
+            return "Product successfully deleted..!"
+    return "Product not found...!"
+
